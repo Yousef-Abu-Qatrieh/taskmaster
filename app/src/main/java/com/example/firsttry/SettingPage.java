1254698027a27b47e21732ee32c43d33807d7137
yousef Abu Qatrieh <97651232+Yousef-Abu-Qatrieh@users.noreply.github.com>
2022-06-28 02:56:43 +0300

    almostDone

--- app/src/main/java/com/example/firsttry/SettingPage.java
+++ app/src/main/java/com/example/firsttry/SettingPage.java
@@ -3,26 +3,55 @@ package com.example.firsttry;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Team;
 
 public class SettingPage extends AppCompatActivity {
     private static final String USERNAME="username";
 private EditText mUsername;
    private Button mSaveButton;
+   Spinner spinner;
+   String teamId = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_page);
         ActionBar actionBar=getSupportActionBar();
         actionBar.setTitle("TaskMaster");
-        mUsername=findViewById(R.id.username);
+        spinner=findViewById(R.id.team_sp)
+;        mUsername=findViewById(R.id.username);
         mSaveButton=findViewById(R.id.btn_save);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                saveUserName();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         mSaveButton.setOnClickListener(view -> {
-            saveUserName();
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor  preferencesEditor = sharedPreferences.edit();
+            preferencesEditor.putString("teamId",teamId);
+            preferencesEditor.apply();
+            finish();
+            System.out.println(teamId+"test++++++++++");
         });
     }
     public void saveUserName() {
@@ -30,8 +59,30 @@ private EditText mUsername;
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor  preferencesEditor = sharedPreferences.edit();
         preferencesEditor.putString(USERNAME,username);
+        String teamname = spinner.getSelectedItem().toString();
+        preferencesEditor.putString("teamName",teamname);
         preferencesEditor.apply();
+        saveTeamId();
 
 
+
+    }
+
+    private void saveTeamId() {
+
+
+        Amplify.API.query(ModelQuery.list(Team.class,Team.NAME.eq(spinner.getSelectedItem().toString())),res->{
+
+
+                for(Team t :res.getData()){
+          if (res.hasData()) {
+             teamId=t.getId();
+          }
+                }
+
+
+        },err->{
+
+        });
     }
 }
